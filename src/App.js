6dfb1ad2763c8a34c6a15b00d13f5dd8c8aa20a0
ToukirIslamShovo <shovoodev@gmail.com>
2023-07-26 14:56:42 +0200

    display country card with api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import CountryCard from "./components/CountryCard";
+import "./app.css"
+import { useEffect, useState } from "react";
+import { getAllCountries } from './services/index.js'
 
 function App() {
+  const [countriesList, setCountriesList] = useState([]);
+  useEffect(() => {
+    getAllCountries().then((result) => {
+      const countries = result.data;
+      setCountriesList(countries);
+    });
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="country-card-wrapper">
+        {
+          countriesList.map(country => (
+
+            <CountryCard
+              key= {country.alpha3Code}
+              name={country.name}
+              capital={country.capital}
+              population={country.population}
+              flagUrl={country.flags.png}
+            />
+          )
+          )
+        }
+      </div>
     </div>
   );
 }
